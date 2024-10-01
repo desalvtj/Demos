d35d7087089c9d31e7695b04f1096d95b026ee41
desalvtj <desalvtj@mail.uc.edu>
2024-10-01 15:12:18 -0400

    three examples

---
+++ src/FormattedOutput.java
@@ -0,0 +1,19 @@
+public class FormattedOutput
+{
+    public static void main(String[] args)
+    {
+        int age = 15;
+        double salary = 45000.236;
+
+        for(int c = 0; c <= 10; c++)
+
+            System.out.printf("%-10s %10.2f %5d\n", "Tom", salary, age);
+
+
+
+            // s - string
+            // f - float
+            // d = digit
+    }
+
+}
